683bdde3ffe2bb30ff1afcbd97f637a3598cf38f
Wagner Ricardo Wagner <wagnerricardonet@Gmail.com>
2019-06-10 08:49:12 -0300

    docs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  /* -- AMOSTRA STATE  state = {
+      meuNome: 'Wagner Ricardo Wagner'
+    }
+  
+    handleState = (e) => {
+      const { value, name } = e.target;
+  
+      this.setState({ [name]: value })
+    }*/
+
+  render() {
+
+
+
+    return (
+      <div id="app" className="container">
+        <h1>FaceHUB</h1>
+        <div className="containerAction">
+          <input type="text" placeholder="Usuário GitHUB" />
+          <button>Buscar</button>
+        </div>
+        <div id="profile" className="containerProfile">
+          <div>
+            <div className="user">
+              <div>
+                <img src="https://avatars0.githubusercontent.com/u/11953187?v=4" />
+              </div>
+              <h4>Wagner Ricardo Wagner e vendo o que acontece</h4>
+              <span>wagnerricardonet@gmail.com.br</span>
+              <span>Meu blog</span>
+              <span>Brasil</span>
+              <span>34 Repositórios</span>
+              <span>5 Seguidores</span>
+            </div>
+          </div>
+          <div className="repositories">
+            <div className="item">
+              <h4>angular-css-grid</h4>
+              <span>exemplo de css grid no angular</span>
+              <a>https://github.com/wagnerww/angular-css-grid</a>
+            </div>
+            <div className="item">
+              <h4>angular-css-grid</h4>
+              <span>exemplo de css grid no angular</span>
+              <a>https://github.com/wagnerww/angular-css-grid</a>
+            </div>
+            <div className="item">
+              <h4>angular-css-grid</h4>
+              <span>exemplo de css grid no angular</span>
+              <a>https://github.com/wagnerww/angular-css-grid</a>
+            </div>
+            <div className="item">
+              <h4>angular-css-grid</h4>
+              <span>exemplo de css grid no angular</span>
+              <a>https://github.com/wagnerww/angular-css-grid</a>
+            </div>
+            <div className="item">
+              <h4>angular-css-grid</h4>
+              <span>exemplo de css grid no angular</span>
+              <a>https://github.com/wagnerww/angular-css-grid</a>
+            </div>
+            <div className="item">
+              <h4>angular-css-grid</h4>
+              <span>exemplo de css grid no angular</span>
+              <a>https://github.com/wagnerww/angular-css-grid</a>
+            </div>
+
+          </div>
+
+        </div>
+        {/* -- AMOSTRA state = meuNome
+        <input type="text" name="meuNome" onChange={handleState} />
+        <button onClick={handleState}>Alterar State</button>
+        */}
+      </div>
+    )
+  }
 }
 
 export default App;
